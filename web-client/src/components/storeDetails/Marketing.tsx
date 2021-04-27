914bb9087997315c5b4861895a023602b55f7918
Fareed Ahmad <fareedahmad@cmail.carleton.ca>
2021-04-27 16:43:16 -0400

    Finish basic ui setup

--- web-client/src/components/storeDetails/Marketing.tsx
+++ web-client/src/components/storeDetails/Marketing.tsx
@@ -1,14 +1,27 @@
-import { Box } from '@material-ui/core';
+import { Box, createStyles, Theme, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
 import React from 'react'
+import { RangeInput } from '../rangeinput.tsx/RangeInput';
 
 interface MarketingProps {
 
 }
 
-export const Marketing: React.FC<MarketingProps> = ({}) => {
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            margin: "50px",
+        },
+    }),
+);
+export const Marketing: React.FC<MarketingProps> = ({ }) => {
+    const classes = useStyles();
     return (
-        <Box>
-            Marketing
+        <Box className={classes.root}>
+            <Typography variant="h5">Marketing</Typography>
+            <RangeInput title="Facebook"></RangeInput>
+            <RangeInput title="Instagram"></RangeInput>
+            <RangeInput title="Google"></RangeInput>
         </Box>
     );
 }
--- web-client/src/components/storeDetails/ProductCategories.tsx
+++ web-client/src/components/storeDetails/ProductCategories.tsx
@@ -1,5 +1,5 @@
 
-import { Box, createStyles, Select, Theme, Typography } from '@material-ui/core';
+import { Box, Container, createStyles, Select, Theme, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
 import React from 'react'
 import CheckboxList from '../checklist/CheckBoxList';
@@ -10,15 +10,14 @@ interface ProductCategoriesProps {
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         root: {
-            flexGrow: 1,
+            margin: "50px",
         },
-
     }),
 );
 export const ProductCategories: React.FC<ProductCategoriesProps> = ({ }) => {
     const classes = useStyles();
     return (
-        <Box>
+        <Box className={classes.root}>
             <Typography variant="h5">Product Categories</Typography>
             <CheckboxList />
         </Box>
